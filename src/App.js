6c2930b8faf6f7e4b3449a763669a932faebde50
Javier Delgado Martinez <javier.delgado@idtconsultores.com>
2022-07-15 14:17:26 -0500

    [App] Se termina app para carrito de compra

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import Productos from './components/productos';
+import Layout from './components/layout';
+import Title from './components/title';
+import NavigationBar from './components/navigation_bar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    productos: [
+      {name: 'Tomate', price: 45,img: '/productos/tomate.jpg'},
+      {name: 'Ejote', price: 12,img: '/productos/arbejas.jpg'},
+      {name: 'Lechuga', price: 36,img: '/productos/lechuga.jpg'},
+    ],
+    carro:[],
+  }
+
+  agregarAlCarrito  = (producto) => {
+    const { carro } = this.state;
+
+    if(carro.find(x => x.name === producto.name)) {
+      const newCarro  = carro.map(x => x.name === producto.name ? ({...x, cantidad: x.cantidad + 1}) : x);
+
+      return this.setState({carro: newCarro});
+    }
+
+    return this.setState({
+      carro: this.state.carro.concat({
+        ...producto,
+        cantidad:1,
+      }),
+    });
+  }
+
+  render() {
+    return (
+      <div>
+      <NavigationBar carro={this.state.carro}/>
+      <Layout>
+      <Title/>
+      <Productos agregarAlCarrito={ this.agregarAlCarrito }
+        productos={ this.state.productos }
+      />
+      </Layout>
+      </div>
+    );
+  }
 }
+
 
 export default App;
